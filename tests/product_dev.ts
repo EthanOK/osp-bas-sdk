9632860537a13ae75dd2fb613fe2fad44bd5660a
DJ.Zhang <dengji@evg.co>
2024-09-23 19:27:51 +0800

    test: tests/product_dev.ts

---
+++ tests/product_dev.ts
@@ -0,0 +1,53 @@
+import { ethers } from "ethers";
+import {
+  encodeAddrToBucketName,
+  GreenFieldClientTS,
+  OspSchemaMap,
+  registerSchema,
+} from "../src";
+
+async function prepareSchemaAndBucket() {
+  // register Schema
+  const signer = new ethers.Wallet(
+    process.env.GREEN_PAYMENT_PRIVATE_KEY!,
+    new ethers.JsonRpcProvider(process.env.BNB_RPC_URL!)
+  );
+  const schemaNames = [];
+  OspSchemaMap.forEach(async (value, key) => {
+    const schemaName = value;
+    schemaNames.push(schemaName);
+  });
+
+  for (let i = 0; i < schemaNames.length; i++) {
+    try {
+      const schemaUID = await registerSchema(
+        signer,
+        process.env.Schema_Registry_BNB!,
+        {
+          schema: schemaNames[i],
+          resolverAddress: "0x0000000000000000000000000000000000000000",
+          revocable: true,
+        }
+      );
+      console.log("schemaUID", schemaUID, "\nschema:", schemaNames[i]);
+    } catch (error) {
+      // console.log(error);
+      continue;
+    }
+  }
+
+  // create bucket
+  try {
+    const client = new GreenFieldClientTS(
+      process.env.GREEN_RPC_URL!,
+      process.env.GREEN_CHAIN_ID!,
+      process.env.GREEN_PAYMENT_ADDRESS!
+    );
+    const isBucketExist = await client.createBucket(
+      encodeAddrToBucketName(process.env.GREEN_PAYMENT_ADDRESS!),
+      process.env.GREEN_PAYMENT_PRIVATE_KEY!
+    );
+  } catch (error) {}
+}
+
+prepareSchemaAndBucket();
